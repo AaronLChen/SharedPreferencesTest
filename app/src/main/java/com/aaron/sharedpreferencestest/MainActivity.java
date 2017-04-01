557fbab2903401890569a07a5afd5d456015a1be
chenlong <chenlongim@gmail.com>
2017-04-01 15:23:53 +0800

    1. 使用SharedPreferences获取存入的数据

--- app/src/main/java/com/aaron/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/aaron/sharedpreferencestest/MainActivity.java
@@ -3,12 +3,14 @@ package com.aaron.sharedpreferencestest;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button saveData;
+    private Button restoreData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         saveData = (Button) findViewById(R.id.save_data);
+        restoreData = (Button) findViewById(R.id.restore_data);
+
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,6 +28,21 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age", 25);
                 editor.putBoolean("married", false);
                 editor.commit();
+            }
+        });
+
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
+
             }
         });
     }
